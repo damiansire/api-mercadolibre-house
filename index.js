28baf4a90bbeac6810c9f8e08a0e41599abc87f5
Damian Sire <damiansire@hotmail.com>
2021-11-14 15:50:36 -0300

    API DUMMY XD

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 
+require("dotenv").config();
+
+const { Client } = require("pg");
+
+const config = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
 app.use(cors());
 
-app.get("/products/:id", function (req, res) {
-  console.log(req);
-  res.send("Hola estoy funcionando xd");
+app.get("/houses/today", async function (req, res) {
+  try {
+    const client = new Client(config);
+    client.connect();
+    const getElementQuery = `SELECT * FROM public.viviendas limit 20`;
+    const { rows } = await client.query(getElementQuery);
+    res.send(rows);
+  } catch {
+    res.send([]);
+  }
 });
 
 app.listen(3001, function () {
